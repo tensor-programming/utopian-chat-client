e39adfd54bd9fa37ab01239803b40733e93dca62
Tensor-Programming <abeltensor@tensor-programming.com>
2018-10-13 14:11:21 -0400

    add comments to state.

--- src/store/state.js
+++ src/store/state.js
@@ -1,10 +1,20 @@
+// state inside of the client store.
+
 export default {
+  // the active user
   user: '',
+  // list of groups or friends.
   list: '',
+  // defines if should be looking at the group or friend lists.
   activeList: 'friends',
+  // the current chat room that is being focused on.
   currentRoom: '',
+  // result of searching actions
   result: '',
+  // should change group/list/private message
   shouldChange: true,
+  // messages in memory.
   messages: [],
+  // counts for users, messages and groups
   count: '',
 }
